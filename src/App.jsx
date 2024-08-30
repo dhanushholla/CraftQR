53d900e6b74b358b2ccd59d6ea40cd9c2862202f
Thirumalaivasan Perumal <dhanushholla.mdh@gmail.com>
2024-08-30 17:26:43 +0530

    fixed loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import './App.css'
-import Myloader from './components/MyLoader'
+import Myloader from './components/Myloader'
 import QRCodeGenerator from './components/QRCodeGenerator'
 import ErrorBoundary from './ErrorBoundary'
 
